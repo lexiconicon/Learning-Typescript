7c32516c5cce145472e000f95e08561f0a5ba0c2
Lexi Velte <lexivelte@gmail.com>
2024-11-02 14:46:51 -0400

    added additional files

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+//TYPE FOR BIRD OBJECT
+export type Bird = { 
+    id: number
+    name: string
+    latin_name: string
+  }
---
+++ src/variables.ts
@@ -0,0 +1,6 @@
+
+//VARIABLES
+export const birdForm = document.getElementById('bird-form') as HTMLFormElement;
+export const deleteBirdsButton = document.getElementById('delete-birds') as HTMLButtonElement;
+export const birdContainer = document.getElementById('bird-container') as HTMLDivElement;
+export const fetchBirdsButton = document.getElementById('fetch-birds') as HTMLButtonElement;
